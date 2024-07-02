85ad9ea8eee3b1925999a86a5560403928bd32d3
AndrewGalak6578 <andrew.galaktionov@inbox.ru>
2024-07-02 14:01:19 +0600

    Models and some changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <title>Title</title>
         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
