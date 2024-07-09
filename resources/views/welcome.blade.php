33de88f2ef150081d801b2ab78366f51581ddfb9
AndrewGalak6578 <andrew.galaktionov@inbox.ru>
2024-07-09 18:57:48 +0600

    Changes by User

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,31 +1,15 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <title>Title</title>
-        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-    <div class="form-control w-25">
-        <div class="mb-3">
-            <label for="formFile" class="form-label">Default file input example</label>
-            <input class="form-control" type="file" id="formFile">
-        </div>
-        <div class="mb-3">
-            <label for="formFileMultiple" class="form-label">Multiple files input example</label>
-            <input class="form-control" type="file" id="formFileMultiple" multiple>
-        </div>
-        <div class="mb-3">
-            <label for="formFileDisabled" class="form-label">Disabled file input example</label>
-            <input class="form-control" type="file" id="formFileDisabled" disabled>
-        </div>
-        <div class="mb-3">
-            <label for="formFileSm" class="form-label">Small file input example</label>
-            <input class="form-control form-control-sm" id="formFileSm" type="file">
-        </div>
-        <div>
-            <label for="formFileLg" class="form-label">Large file input example</label>
-            <input class="form-control form-control-lg" id="formFileLg" type="file">
-        </div>
-    </div>
-    </body>
+<head>
+    <title>Title</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body class="font-sans antialiased dark:bg-black dark:text-white/50">
+<div class="container-fluid text-center bg-body-secondary">
+    <p class="fw-bold p-sm-2">Consumer Advisory: APR Rates Range From 5.99% to 35.99% Maximum APR for qualified consumers.</p>
+</div>
+<nav>
+
+</nav>
+</body>
 </html>
